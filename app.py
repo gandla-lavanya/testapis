7ded30d06895b93ee505fff6f82b124d5f25e73f
lavanya <lavanya2572@gmail.com>
2020-06-12 10:53:52 +0530

    heroku testing

--- app.py
+++ app.py
@@ -1,31 +1,8 @@
-from flask import Flask,jsonify
+from flask import Flask
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
-from resources.emp import Emp,EmpLogin
-
+from resources.emp import Emp
 app=Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS']=True
-app.config['JWT_SECRET_KEY']='coscskillup'
 api=Api(app)
-jwt=JWTManager(app)
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return jsonify({
-        'error': 'authorization_required',
-        "description": "Request does not contain an access token."
-    }), 401
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({
-        'error': 'invalid_token',
-        'message': 'Signature verification failed.'
-    }), 401
-
-
 api.add_resource(Emp,'/emp')
-api.add_resource(EmpLogin,'/login')
-
 if __name__=='__main__':
     app.run()
