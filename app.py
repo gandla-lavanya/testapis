14cbbda5998f37516728bff3e178a43ef88ee252
lavanya <lavanya2572@gmail.com>
2020-06-12 11:28:52 +0530

    adding change

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from resources.emp import Emp
+
 app=Flask(__name__)
 api=Api(app)
+
 api.add_resource(Emp,'/emp')
+
 if __name__=='__main__':
     app.run()
